bf1b784195e4a3f1445f222b435a6a0b02d748eb
Kevinjohn Gallagher <kevinjohngallagher@hotmail.com>
2015-04-04 18:24:48 +0100

    Initial commit

---
+++ src/Kevinjohn/L5BootCamp/L5BootCampServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Kevinjohn\L5BootCamp;
+
+use Illuminate\Support\ServiceProvider;
+
+class L5BootCampServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return [];
+	}
+
+}
