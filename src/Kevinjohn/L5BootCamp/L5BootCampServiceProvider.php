fdc5d2083d64fa320a0f53dff0b67d43dd567251
Kevinjohn Gallagher <kevinjohngallagher@hotmail.com>
2015-04-04 20:16:18 +0100

    Skeleton package set up

--- src/Kevinjohn/L5BootCamp/L5BootCampServiceProvider.php
+++ src/Kevinjohn/L5BootCamp/L5BootCampServiceProvider.php
@@ -21,6 +21,50 @@ class L5BootCampServiceProvider extends ServiceProvider {
 		//
 	}
 
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //  Include the routes file
+        include __DIR__.'/app/http/routes/routes.php';
+
+
+
+        //  Publishing config to laravel
+        $this->publishes([
+            __DIR__.'/config/L5BootCamp.php' => config_path('L5BootCamp.php.php'),
+        ], 'config');
+
+
+        // Publish your migrations
+        $this->publishes([
+            __DIR__.'/database/migrations/' => base_path('/database/migrations')
+        ], 'migrations');
+
+
+
+        //registering Views Folder
+        $this->loadViewsFrom(
+            __DIR__.'/resources/views', 'L5BootCamp'
+        );
+
+        //  Publishing views to laravel
+        $this->publishes([
+            __DIR__.'/resources/views' => base_path('resources/views/vendor/L5BootCamp'),
+        ], 'views');
+
+
+        //  Publishing assets to laravel
+        $this->publishes([
+            __DIR__.'/resources/assets' => public_path('vendor/L5BootCamp/assets'),
+        ], 'assets');
+    }
+
+
 	/**
 	 * Get the services provided by the provider.
 	 *
---
+++ src/Kevinjohn/L5BootCamp/app/http/routes/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+    Route::group('{version}/{language-prefix}/portal/', function(){
+
+    });
+
+
+    Route::group('{version}/{language-prefix}/api/', function(){
+
+    });
