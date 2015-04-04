aa63e447d344f867da99acce7a199c5b95a75644
Kevinjohn Gallagher <kevinjohngallagher@hotmail.com>
2015-04-04 20:25:05 +0100

    Config update with some basics

--- src/Kevinjohn/L5BootCamp/config/L5BootCamp.php
+++ src/Kevinjohn/L5BootCamp/config/L5BootCamp.php
@@ -1,1 +1,11 @@
 <?php
+namespace Kevinjohn\L5BootCamp\config;
+
+    return [
+        "author"    =>  "Kevinjohn Gallagher",
+        "package"   =>  "L5BootCamp",
+        "version"   =>  "0.1",
+        "GitHub"    =>  "Kevinjohn/L5Bootcamp"
+    ];
+
+
